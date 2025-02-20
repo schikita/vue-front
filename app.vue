11bd0b8c6635481c32f1d135e872b94d3a5b6cc7
Dzubei <chikita@sb.by>
2025-02-20 11:12:47 +0300

    connection font in project

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup>
-import Header from './components/Header.vue';
+import Header from './components/Header.vue'; 
 </script>
 
 <style scoped>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,7 @@ export default defineNuxtConfig({
       ],
     },
   },
+  css: ['@/assets/styles/app.scss'],
   runtimeConfig: {
     gwEndPoint: "",
 
